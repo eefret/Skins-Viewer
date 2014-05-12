1ba8e7ea80fd925a40bdbfdb7861f484ca1cef4c
Christopher Lee <slee2714@gmail.com>
2014-05-11 23:28:51 -0500

    tab spacing inconsistencies

--- src/com/chrslee/csgoskins/MainActivity.java
+++ src/com/chrslee/csgoskins/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-	
+    
     Button riflesButton;
     Button smgsButton;
     Button heavyButton;
@@ -25,86 +25,86 @@ public class MainActivity extends Activity {
     Button mapsButton;
     Button casesButton;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		riflesButton = (Button) findViewById(R.id.rifles_button);
-	    riflesButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, Rifles.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	    
-		smgsButton = (Button) findViewById(R.id.smgs_button);
-	    smgsButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, SMGs.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	    
-		heavyButton = (Button) findViewById(R.id.heavy_button);
-	    heavyButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, Heavy.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	    
-		pistolsButton = (Button) findViewById(R.id.pistols_button);
-	    pistolsButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, Pistols.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	    
-		knivesButton = (Button) findViewById(R.id.knives_button);
-	    knivesButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, Knives.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	    
-	    mapsButton = (Button) findViewById(R.id.maps_button);
-	    mapsButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, Maps.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	    
-	    casesButton = (Button) findViewById(R.id.cases_button);
-	    casesButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent intent = new Intent(MainActivity.this, Cases.class);
-	    		startActivity(intent);
-	    	}
-	    });
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        riflesButton = (Button) findViewById(R.id.rifles_button);
+        riflesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Rifles.class);
+                startActivity(intent);
+            }
+        });
+        
+        smgsButton = (Button) findViewById(R.id.smgs_button);
+        smgsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SMGs.class);
+                startActivity(intent);
+            }
+        });
+        
+        heavyButton = (Button) findViewById(R.id.heavy_button);
+        heavyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Heavy.class);
+                startActivity(intent);
+            }
+        });
+        
+        pistolsButton = (Button) findViewById(R.id.pistols_button);
+        pistolsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Pistols.class);
+                startActivity(intent);
+            }
+        });
+        
+        knivesButton = (Button) findViewById(R.id.knives_button);
+        knivesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Knives.class);
+                startActivity(intent);
+            }
+        });
+        
+        mapsButton = (Button) findViewById(R.id.maps_button);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Maps.class);
+                startActivity(intent);
+            }
+        });
+        
+        casesButton = (Button) findViewById(R.id.cases_button);
+        casesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Cases.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
