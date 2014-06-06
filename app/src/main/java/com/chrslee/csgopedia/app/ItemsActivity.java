71975979c9be18195cca595067ed812c31d4f1ca
Marvin Yan <marvin.yan@outlook.com>
2014-06-05 23:21:34 -0400

    Modify ListView adapter

--- app/src/main/java/com/chrslee/csgopedia/app/ItemsActivity.java
+++ app/src/main/java/com/chrslee/csgopedia/app/ItemsActivity.java
@@ -1,48 +1,42 @@
 package com.chrslee.csgopedia.app;
 
-import android.app.ProgressDialog;
+import android.app.ListActivity;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chrslee.csgopedia.app.util.Item;
-import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 
-public class ItemsActivity extends ActionBarActivity {
+public class ItemsActivity extends ListActivity {
     private List<Item> myItems = new ArrayList<Item>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_items);
 
         Bundle extras = getIntent().getExtras();
         String itemType = extras.getString("itemType");
 
         populateListWith(itemType);
-        populateListView();
+        setListAdapter(new MobileArrayAdapter(this));
 
         // Change title
-        getSupportActionBar().setTitle(itemType);
-        registerClickCallback();
+        getActionBar().setTitle(itemType + " List");
     }
 
     //Populate the arraylist with all of the weapons
@@ -66,57 +60,34 @@ public class ItemsActivity extends ActionBarActivity {
         cursor.close();
     }
 
-    private void populateListView() {
-        ArrayAdapter<Item> adapter = new MyListAdapter(); //Array adapter for Item objects
-        ListView list = (ListView) findViewById(R.id.items_listView); //Set up list
-        list.setAdapter(adapter); //assign adapter to ListView
-    }
+    /**
+     * http://www.mkyong.com/android/android-listview-example/
+     */
+    public class MobileArrayAdapter extends ArrayAdapter<Item> {
+        private final Context context;
 
-    //finish this. open new activity when an item is clicked
-    private void registerClickCallback() {
-        ListView list = (ListView) findViewById(R.id.items_listView);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-                Item clickedRifle = myItems.get(position);
-            }
-        });
-    }
-
-    //Adapter for listview. Adapter inflates the layout for each row in its getview() method
-    //and assigns the data to the individual views in the row. adapter takes an Array and converts the items into View objects to be loaded into the ListView container.
-    private class MyListAdapter extends ArrayAdapter<Item> {
-        public MyListAdapter() { //constructor
-            super(ItemsActivity.this, R.layout.item_view, myItems);
+        public MobileArrayAdapter(Context context) {
+            super(context, R.layout.item_view, myItems);
+            this.context = context;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            //Make sure we have a view to work with (may have been given null)
-            View itemView = convertView; //?
-            if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
-            }
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-            //Find the rifle to work with
             Item currentItem = myItems.get(position);
+            View rowView = inflater.inflate(R.layout.item_view, parent, false);
 
-            //Fill the view
-            ImageView imageView = (ImageView) itemView.findViewById(R.id.item_icon); //Find icon view
-            imageView.setImageResource(currentItem.getIconID());
+            ImageView icon = (ImageView) rowView.findViewById(R.id.item_icon);
+            TextView name = (TextView) rowView.findViewById(R.id.item_name);
+            TextView price = (TextView) rowView.findViewById(R.id.item_price);
 
-            // or imageView.setImageDrawable(getResources().getDrawable(currentItem.getIconID()));
-            // setImageResource reads and decodes Bitmap on the UI thread
+            icon.setImageResource(currentItem.getIconID());
+            name.setText(currentItem.getItemName());
+            price.setText(currentItem.getPrice());
 
-            //Rifle name:
-            TextView nameText = (TextView) itemView.findViewById(R.id.item_name);
-            nameText.setText(currentItem.getItemName());
-
-            //Rifle price:
-            TextView priceText = (TextView) itemView.findViewById(R.id.item_price);
-            priceText.setText(currentItem.getPrice());
-
-            return itemView;
+            return rowView;
         }
     }
 
@@ -133,9 +104,14 @@ public class ItemsActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        //get selected items
+        String selectedValue = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+    }
+
 }
