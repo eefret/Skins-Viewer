80e35525997216d6e77c62ad48eaad7377abf2cf
Christopher T. Herrera <christopher.h@futurelinkcorporation.com>
2015-09-19 22:45:34 -0400

    commenting #5

--- app/src/main/java/com/chrslee/csgopedia/app/CardFragment.java
+++ app/src/main/java/com/chrslee/csgopedia/app/CardFragment.java
@@ -85,48 +85,62 @@ public class CardFragment extends Fragment {
         //setting the params from above
 
         // First - image section
+        //Building the imageview via code
         LayoutParams imgParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         imgParams.gravity = Gravity.CENTER;
+        //setting all centered
         ImageView iv = new ImageView(context);
         iv.setLayoutParams(imgParams);
         iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         iv.setAdjustViewBounds(true);
         iv.setBackgroundResource(getActivity().getIntent().getExtras().getInt("iconID"));
+        //seting the iconID as the image of this imageView
 
         // Second - prices section
         //final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources()
         //        .getDisplayMetrics());
+        //
 
+        //LinearLayout is another container it just take the
+        //widgets inside it and display them in a linear order
         LinearLayout linLayout = new LinearLayout(context);
         linLayout.setOrientation(LinearLayout.VERTICAL);
         linLayout.setLayoutParams(params);
         linLayout.setGravity(Gravity.CENTER);
 
         // Get prices
-        ConnectionDetector cd = new ConnectionDetector(context);
-        String query = getActivity().getIntent().getStringExtra("searchQuery");
-        TextView placeholder = new TextView(context);
-        if (cd.isConnectedToInternet()) {
-            if (query.equals("-1")) {
+        ConnectionDetector cd = new ConnectionDetector(context); //creating a connection detector to check for internet
+        String query = getActivity().getIntent().getStringExtra("searchQuery"); //taking the name or whatever he pass from mainactivity from the searchquery arg
+        TextView placeholder = new TextView(context); ///this is a textview for the name of the item
+        if (cd.isConnectedToInternet()) {//if its connected to internet then
+            if (query.equals("-1")) {//if the skin is regular he display is not for sale
                 placeholder.setText("Regular skins are not for sale!");
                 linLayout.addView(placeholder);
-            } else {
+            } else {//else he calls the scrappperAsyncTask with the query
                 new ScraperAsyncTask(linLayout).execute(query);
             }
-        } else {
+        } else {//if its not connected he display the message here
             placeholder.setText("Please connect to the Internet to view prices.");
             linLayout.addView(placeholder);
         }
 
+        //here he defines which tab he is displaying, Now I see why he used the same fragment,
+        //this is a bad practice, he created both widgets before but just displays one of them
+        //for each case if its the first tab he display the image if its the second the prices
         if (position == 0) {
             fl.addView(iv);
         } else {
             fl.addView(linLayout);
         }
+
+        //Then he returns the framelayout (container) to display it in the screen
         return fl;
     }
 
     public enum Quality {
+        //Enum with the quality, what a silly he just had to take the Enum.toString() and
+        //replace the _ with spaces and do a camelCase parsing, instead of hardcoding the
+        //same enum name as string -_-
         ST_FACTORY_NEW("ST Factory New"),
         ST_MINIMAL_WEAR("ST Minimal Wear"),
         ST_FIELD_TESTED("ST Field-Tested"),
@@ -154,6 +168,11 @@ public class CardFragment extends Fragment {
      * This task gets the starting prices for all qualities of a skin.
      */
     private class ScraperAsyncTask extends AsyncTask<String, Void, HashMap<String, Double>> {
+        //Will explain what an async task is, network request cant be done in the UI thread
+        //(the main one) so we need to execute those requests in a separate thread
+        //that is async task for, thus is not a good practice to use them for this because
+        //asynctask live inside the activity lifecycle so if the activity goes onpause
+        //while the asynctask is executing it will die and start again -_-
         LinearLayout l;
         double rate;
         boolean isAutoDetected;
@@ -161,6 +180,8 @@ public class CardFragment extends Fragment {
         TextView loading;
 
         protected ScraperAsyncTask(LinearLayout linLayout) {
+            //Constructor for the asyncTask takes the parent linear layout as parameter to
+            //set a loading... message in the meantime the request is executing
             l = linLayout;
             loading = new TextView(context);
             loading.setText("Loading...");
@@ -172,6 +193,7 @@ public class CardFragment extends Fragment {
         protected HashMap<String, Double> doInBackground(String... params) {
             isAutoDetected = sharedPrefs.getBoolean("auto_detect_locale", true);
             symbol = sharedPrefs.getString("custom_currency", "USD");
+            //getting a boolean and a string from prefs
 
             // Auto locale detection on/off
             if (isAutoDetected) {
@@ -179,7 +201,7 @@ public class CardFragment extends Fragment {
             } else {
                 rate = new CurrencyRates(context).getRate(symbol);
             }
-
+            //Executing the PriceScrapper and returning the hashmap it returns
             return PriceScraper.getPrices(params[0]);
         }
 
@@ -189,11 +211,17 @@ public class CardFragment extends Fragment {
 
         @Override
         protected void onPostExecute(HashMap<String, Double> priceData) {
-            l.removeAllViewsInLayout();
+            //this executes after the scrapper has finished and returns his data
+            l.removeAllViewsInLayout();//removes all view from the linear container he passed
+            ///in the params of the asynctask
+            //this also remove the loading... message
 
             // See SpannableStrings if multi-colored text desired in the text
             // (e.g. StatTrak orange, prices green)
-            for (Quality q : Quality.values()) {
+
+            //Now displays all  prices and name with the color of their rarity in the
+            //linear passed in the params
+            for (Quality q : Quality.values()) {//iterating through prices to display them
                 TextView price = new TextView(context);
                 price.setGravity(Gravity.CENTER_HORIZONTAL);
 
